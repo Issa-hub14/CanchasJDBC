f86cd84e0c0fc97632c0bb73a4aeb1d14e622bdd
Issa-hub14 <isabellaquijano1@gmail.com>
2026-05-10 00:54:18 -0500

    Creación del proyecto para manejo de canchas

---
+++ src/main/java/com/mycompany/canchasjdbc/CanchasJDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.canchasjdbc;
+
+/**
+ *
+ * @author isabe
+ */
+public class CanchasJDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
